5345bdf9ef19a26f0690277562141c32215fcc1e
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-12-02 10:45:56 +0200

    module calculates Pascal Tri

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Pascal's Triangle"""
 
+
 def pascal_triangle(n):
     """Returns a list of lists of integers
     representing the Pascal's triangle of (n)"""
@@ -8,9 +9,9 @@ def pascal_triangle(n):
         return list()
 
     triangle = [[1]]
-    for r in range(1,n):
+    for r in range(1, n):
         row = [1]
-        for c in range(1,r):
+        for c in range(1, r):
             row.append(triangle[r-1][c] + triangle[r-1][c-1])
         row.append(1)
         triangle.append(row)
