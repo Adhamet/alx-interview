0169edb8e247eda1c7838c36c2b621f4b62cdb12
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-12-07 14:03:49 +0200

    pycodesty'e

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,6 +7,7 @@ Problem: You have n number of locked boxes in front of you.
 Task: Write a method that determines if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """
     Solution of the problem:
@@ -21,6 +22,6 @@ def canUnlockAll(boxes):
             boxesChecked = k in boxes[idx] and k != idx
             if boxesChecked:
                 break
-        if boxesChecked == False:
+        if boxesChecked is False:
             return boxesChecked
     return True
